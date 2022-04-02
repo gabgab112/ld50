9d5b31f8f6255e631e9b7e579612067def284167
Gabriel B <gabrielbissonnette36@gmail.com>
2022-04-01 23:01:18 -0400

    Updated sunset pack

---
+++ Assets/GabrielBissonnette/2D Sunsets Pack/Editor/LayerEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LayerManager))]
+public class LayerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        LayerManager layerManager = (LayerManager)target;
+
+        layerManager.ChangeColor();
+    }
+}
---
+++ Assets/GabrielBissonnette/2D Sunsets Pack/Scripts/LayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerManager : MonoBehaviour
+{
+    [Header("Layer Color")]
+    [Tooltip("The color of the layer")]
+    public Color32 layerColor;
+
+    [Header("Objects")]
+    [Tooltip("Objects that will be affected by the color change")]
+    public SpriteRenderer[] subjectToColorChange;
+
+    public void ChangeColor()
+    {
+        if(subjectToColorChange != null && subjectToColorChange.Length >= 1)
+        {
+            foreach(var sprite in subjectToColorChange)
+            {
+                if (sprite != null)
+                    sprite.color = layerColor;
+            }
+        }
+    }
+}
